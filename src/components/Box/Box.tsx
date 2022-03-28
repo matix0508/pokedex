bdb6425da9979b545b5c9060502d683c48e18971
Mateusz Wlekliński <wleklinskimateusz05@gmail.com>
2022-03-28 17:51:10 +0200

    update colors, fonts

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -2,19 +2,24 @@ import React, { FC } from "react";
 import { Pokemon } from "../../types/Pokemon";
 import styles from "./Box.module.scss";
 import { BoxItem } from "../BoxItem/BoxItem";
+import classNames from "classnames";
+import { useAppSelector } from "../../app/hooks";
+import { RootState } from "../../app/store";
 
 interface IBox {
   pokemon: Pokemon;
 }
 
 export const Box: FC<IBox> = ({ pokemon }) => {
+
+  const dark = useAppSelector((state: RootState) => state.darkMode.dark)
   return (
-    <div className={styles.Box}>
+    <div className={classNames([styles.Box, {primary: !dark, "primary-dark": dark}])}>
       <div className={styles.Box__header}>
         <img src={pokemon.sprite} width={150} alt={pokemon.name} />
         {pokemon.name}
         </div>
-      <div className={styles.Box__body}>
+      <div className={classNames([styles.Box__body])}>
         
         <ul>
           <BoxItem label="Type">
